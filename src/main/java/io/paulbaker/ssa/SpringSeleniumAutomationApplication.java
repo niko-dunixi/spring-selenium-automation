828546ee4839d36cc07eb1e1deab4770c0285cda
Paul Baker <paul.baker@overstock.com>
2015-09-24 15:18:41 -0600

    Adding base project files

---
+++ src/main/java/io/paulbaker/ssa/SpringSeleniumAutomationApplication.java
@@ -0,0 +1,12 @@
+package io.paulbaker.ssa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeleniumAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeleniumAutomationApplication.class, args);
+    }
+}
